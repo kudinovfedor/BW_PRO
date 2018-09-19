0f350a833da42d8b3c04b2ed15fe18e3e7e1f167
Kudinov Fedor <brothersrabbits@mail.ru>
2018-09-19 16:15:18 +0300

    Small fix;

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no, user-scalable=yes">
     <?php
     define('BW_IMG', get_template_directory_uri() . '/assets/img');
     define('BW_FAVICON', BW_IMG . '/favicon');
